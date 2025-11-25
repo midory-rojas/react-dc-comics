acddfa270cf0d4962d059e1a3ee21fb01aa5dd13
“midory-rojas” <midory.rojasv@gmail.com>
2025-11-25 16:25:27 +0100

    feat: header aggiunto

---
+++ src/components/Header.jsx
@@ -0,0 +1,44 @@
+import Logo from "../assets/img/dc-logo.png";
+import style from "./Header.module.css";
+
+export default function Header() {
+    return (
+        <header className="d-flex align-center justify-content-center">
+            <img src={Logo} alt="" />
+            <nav className={style.nav}>
+                <ul className="py-20">
+                    <li>
+                        <a href="">CHARACTERS</a>
+                    </li>
+                    <li className={style.active}>
+                        <a href="">COMICS</a>
+                    </li>
+                    <li>
+                        <a href="">MOVIES</a>
+                    </li>
+                    <li>
+                        <a href="">TV</a>
+                    </li>
+                    <li>
+                        <a href="">GAMES</a>
+                    </li>
+                    <li>
+                        <a href="">COLLECTIBLES</a>
+                    </li>
+                    <li>
+                        <a href="">VIDEOS</a>
+                    </li>
+                    <li>
+                        <a href="">FANS</a>
+                    </li>
+                    <li>
+                        <a href="">NEWS</a>
+                    </li>
+                    <li>
+                        <a href="">SHOP</a>
+                    </li>
+                </ul>
+            </nav>
+        </header>
+    );
+}
