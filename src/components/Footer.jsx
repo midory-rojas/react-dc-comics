5c7c07bc9e7a2774336a94fbe2e60259ac9ca5a4
“midory-rojas” <midory.rojasv@gmail.com>
2025-11-26 17:15:26 +0100

    feat: ho aggiunto dinamicamentre le card con map

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -3,41 +3,42 @@ import Facebook from "./Social";
 export default function Footer() {
     return (
         <footer>
+            <section className="footer-top">
                 <div className={`py-40 ${style.background}`}>
                     <div className="d-flex container">
                         <div className={style.col}>
                             <h3 className="color-white">DC COMICS</h3>
                             <ul className="d-flex flex-direction-column mt-20">
                                 <li>
-                                    <a href>Characters</a>
+                                    <a href="">Characters</a>
                                 </li>
                                 <li>
-                                    <a href>Comics</a>
+                                    <a href="">Comics</a>
                                 </li>
                                 <li>
-                                    <a href>Movies</a>
+                                    <a href="">Movies</a>
                                 </li>
                                 <li>
-                                    <a href>TV</a>
+                                    <a href="">TV</a>
                                 </li>
                                 <li>
-                                    <a href>Games</a>
+                                    <a href="">Games</a>
                                 </li>
                                 <li>
-                                    <a href>Videos</a>
+                                    <a href="">Videos</a>
                                 </li>
                                 <li>
-                                    <a href>News</a>
+                                    <a href="">News</a>
                                 </li>
                             </ul>
                             <div className="mt-20">
                                 <h3 className="color-white ">SHOP</h3>
                                 <ul className="d-flex flex-direction-column mt-20">
                                     <li>
-                                        <a href>Shop DC</a>
+                                        <a href="">Shop DC</a>
                                     </li>
                                     <li>
-                                        <a href>Shop DC Collectibles</a>
+                                        <a href="">Shop DC Collectibles</a>
                                     </li>
 
                                 </ul>
@@ -47,37 +48,37 @@ export default function Footer() {
                             <h3 className="color-white">DC</h3>
                             <ul className="d-flex flex-direction-column mt-20">
                                 <li>
-                                    <a href>Terms of Use</a>
+                                    <a href="">Terms of Use</a>
                                 </li>
                                 <li>
-                                    <a href>Privacy policy (New)</a>
+                                    <a href="">Privacy policy (New)</a>
                                 </li>
                                 <li>
-                                    <a href>Ad Choices</a>
+                                    <a href="">Ad Choices</a>
                                 </li>
                                 <li>
-                                    <a href>Advertising</a>
+                                    <a href="">Advertising</a>
                                 </li>
                                 <li>
-                                    <a href>Jobs</a>
+                                    <a href="">Jobs</a>
                                 </li>
                                 <li>
-                                    <a href>Subscriptions</a>
+                                    <a href="">Subscriptions</a>
                                 </li>
                                 <li>
-                                    <a href>Talent Workshops</a>
+                                    <a href="">Talent Workshops</a>
                                 </li>
                                 <li>
-                                    <a href>CPSC Certificates</a>
+                                    <a href="">CPSC Certificates</a>
                                 </li>
                                 <li>
-                                    <a href>Ratings</a>
+                                    <a href="">Ratings</a>
                                 </li>
                                 <li>
-                                    <a href>Shop Help</a>
+                                    <a href="">Shop Help</a>
                                 </li>
                                 <li>
-                                    <a href>Contact Us</a>
+                                    <a href="">Contact Us</a>
                                 </li>
                             </ul>
                         </div>
@@ -85,19 +86,19 @@ export default function Footer() {
                             <h3 className="color-white">SITES</h3>
                             <ul className="d-flex flex-direction-column mt-20">
                                 <li>
-                                    <a href>DC</a>
+                                    <a href="">DC</a>
                                 </li>
                                 <li>
-                                    <a href>MAD Magazine</a>
+                                    <a href="">MAD Magazine</a>
                                 </li>
                                 <li>
-                                    <a href>CD Kids</a>
+                                    <a href="">CD Kids</a>
                                 </li>
                                 <li>
-                                    <a href>DC Universe</a>
+                                    <a href="">DC Universe</a>
                                 </li>
                                 <li>
-                                    <a href>DC Power Visa</a>
+                                    <a href="">DC Power Visa</a>
                                 </li>
                             </ul>
                         </div>
@@ -106,27 +107,29 @@ export default function Footer() {
                         </div>
                     </div>
                 </div>
-
-                <div className={`py-20 ${style.footer}`}>
-                    <div className="container d-flex justify-content-space-between align-center">
-                        <div className={style.btn}>SIGN-UP NOW!</div>
+            </section>
+            <section className="footer-bottom">
+            <div className={`py-20 ${style.footer}`}>
+                <div className="container d-flex justify-content-space-between align-center">
+                    <div className={style.btn}>SIGN-UP NOW!</div>
                     <div>
                         <h4>FOLLOW US</h4>
                         <ul>
                             <li>
-                                <a href>
+                                <a href="">
                                     <Facebook />
                                 </a>
                             </li>
                             <li>
-                                <a href>
+                                <a href="">
 
                                 </a>
                             </li>
                         </ul>
                     </div>
-                    </div>
                 </div>
+            </div>
+            </section>
         </footer>
     );
 }
--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -1,10 +1,20 @@
 import style from "./Main.module.css";
+import { comics } from "../assets/dc-comics-2/comics"
 
 export default function Main() {
+    console.log(comics);
     return (
         <main className="py-40">
             <div className="container">
-                <h2>Content goes here</h2>
+                <h2>Current Series</h2>
+                <div className="d-flex wrap gap-10">
+                    {comics.map((curComic) => (
+                        <div className="col" key={curComic.id}>
+                            <img src={curComic.thumb} alt="" />
+                            <h3>{curComic.series}</h3>
+                        </div>
+                    ))}
+                </div>
             </div>
         </main>
     );
