2db26bdfbdfb6e1e99dcb4666a144a3aee2d2a51
“midory-rojas” <midory.rojasv@gmail.com>
2025-11-26 18:40:41 +0100

    feat: ho creato la nav dinamicamente con Map

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -2,42 +2,19 @@ import Logo from "../assets/img/dc-logo.png";
 import style from "./Header.module.css";
 
 export default function Header() {
+    const links = ["CHARACTERS", "COMICS", "MOVIES", "TV", "GAMES", "COLLECTIBLES", "VIDEOS", "FANS", "NEWS", "SHOP"];
     return (
         <header className="py-10">
             <div className="container d-flex justify-content-space-between align-center">
                 <img className="logo"src={Logo} alt="Logo DC Comics" />
+                {/* Creo dinamicamente i link con map */}
                 <nav className={style.nav}>
                     <ul className="py-10">
-                        <li>
-                            <a href="">CHARACTERS</a>
+                    {links.map((curLink, index) => (
+                        <li key={index}>
+                            <a href="">{curLink}</a>
                         </li>
-                        <li className={style.active}>
-                            <a href="">COMICS</a>
-                        </li>
-                        <li>
-                            <a href="">MOVIES</a>
-                        </li>
-                        <li>
-                            <a href="">TV</a>
-                        </li>
-                        <li>
-                            <a href="">GAMES</a>
-                        </li>
-                        <li>
-                            <a href="">COLLECTIBLES</a>
-                        </li>
-                        <li>
-                            <a href="">VIDEOS</a>
-                        </li>
-                        <li>
-                            <a href="">FANS</a>
-                        </li>
-                        <li>
-                            <a href="">NEWS</a>
-                        </li>
-                        <li>
-                            <a href="">SHOP</a>
-                        </li>
+                    ))}
                     </ul>
                 </nav>
             </div>
