ca234e29ccd01e7c733514db477812680ee6ce8d
“midory-rojas” <midory.rojasv@gmail.com>
2025-11-27 18:03:20 +0100

    feat:footer top fatto con map

---
+++ src/assets/dc-comics-2/linksfooter.js
@@ -0,0 +1,110 @@
+const dcComicsLinks = [
+        {
+            title: "Characters",
+            path: "/characters",
+        },
+        {
+            title: "Comics",
+            path: "./comics",
+        },
+        {
+            title: "Movies",
+            path: "./movies",
+        },
+        {
+            title: "TV",
+            path: "./tv",
+        },
+        {
+            title: "Games",
+            path: "./games",
+        },
+        {
+            title: "Videos",
+            path: "./videos",
+        },
+        {
+            title: "News",
+            path: "./News",
+        }
+    ];
+    const shop = [
+        {
+            title: "Shop DC",
+            path: "./shop",
+        },
+        {
+            title:"Shop DC Collectibles",
+            path:"./shopcollectibles"
+        }
+    ];
+    const dcLinks = [
+        {
+            title: "Terms of Use",
+            path: "/termsofuse",
+        },
+        {
+            title: "Privacy policy New",
+            path: "./privacypolicynew",
+        },
+        {
+            title: "Ad Choices",
+            path: "./adchoices",
+        },
+        {
+            title: "Advertising",
+            path: "./advertising",
+        },
+        {
+            title: "Jobs",
+            path: "./jobs",
+        },
+        {
+            title: "Subscriptions",
+            path: "./suscriptions",
+        },
+        {
+            title: "Talent Workshops",
+            path: "./talentWorkshops",
+        },
+        {
+            title: "CPSC Certificates",
+            path: "./certificates",
+        },
+        {
+            title: "Ratings",
+            path: "./ratings",
+        },
+        {
+            title: "Shop Help",
+            path: "./shop",
+        },
+        {
+            title: "Contact us",
+            path: "./contact"
+        }
+    ];
+     const sitesLinks = [
+        {
+            title: "DC",
+            path: "/dc",
+        },
+        {
+            title: "MAD Magazine",
+            path: "./madmagazine",
+        },
+        {
+            title: "DC Kids",
+            path: "./dckids",
+        },
+        {
+            title: "DC Universe",
+            path: "./dcuniverse",
+        },
+        {
+            title: "DC Power Visa",
+            path: "./dcpowervisa",
+        }  
+     ];
+
+     export {dcComicsLinks, shop, dcLinks, sitesLinks};
--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,5 +1,7 @@
 import style from "./Footer.module.css";
 import Facebook from "./Social";
+import {dcComicsLinks, shop, dcLinks, sitesLinks} from "../assets/dc-comics-2/linksfooter";
+
 export default function Footer() {
     return (
         <footer>
@@ -9,97 +11,41 @@ export default function Footer() {
                         <div className={style.col}>
                             <h3 className="color-white">DC COMICS</h3>
                             <ul className="d-flex flex-direction-column mt-20">
-                                <li>
-                                    <a href="">Characters</a>
-                                </li>
-                                <li>
-                                    <a href="">Comics</a>
-                                </li>
-                                <li>
-                                    <a href="">Movies</a>
-                                </li>
-                                <li>
-                                    <a href="">TV</a>
-                                </li>
-                                <li>
-                                    <a href="">Games</a>
-                                </li>
-                                <li>
-                                    <a href="">Videos</a>
-                                </li>
-                                <li>
-                                    <a href="">News</a>
-                                </li>
+                                {dcComicsLinks.map((link, index) => (
+                                    <li key={index}>
+                                        <a href={link.path}>{link.title}</a>
+                                    </li>
+                                ))}
                             </ul>
                             <div className="mt-20">
                                 <h3 className="color-white ">SHOP</h3>
                                 <ul className="d-flex flex-direction-column mt-20">
-                                    <li>
-                                        <a href="">Shop DC</a>
-                                    </li>
-                                    <li>
-                                        <a href="">Shop DC Collectibles</a>
-                                    </li>
-
+                                    {shop.map((link, index) => (
+                                        <li key={index}>
+                                            <a href={link.path}>{link.title}</a>
+                                        </li>
+                                    ))}
                                 </ul>
                             </div>
                         </div>
                         <div className={style.col}>
-                            <h3 className="color-white">DC</h3>
+                            <h3 className="color-white ">DC</h3>
                             <ul className="d-flex flex-direction-column mt-20">
-                                <li>
-                                    <a href="">Terms of Use</a>
-                                </li>
-                                <li>
-                                    <a href="">Privacy policy (New)</a>
-                                </li>
-                                <li>
-                                    <a href="">Ad Choices</a>
-                                </li>
-                                <li>
-                                    <a href="">Advertising</a>
-                                </li>
-                                <li>
-                                    <a href="">Jobs</a>
-                                </li>
-                                <li>
-                                    <a href="">Subscriptions</a>
-                                </li>
-                                <li>
-                                    <a href="">Talent Workshops</a>
-                                </li>
-                                <li>
-                                    <a href="">CPSC Certificates</a>
-                                </li>
-                                <li>
-                                    <a href="">Ratings</a>
-                                </li>
-                                <li>
-                                    <a href="">Shop Help</a>
-                                </li>
-                                <li>
-                                    <a href="">Contact Us</a>
-                                </li>
+                                {dcLinks.map((link, index) => (
+                                    <li key={index}>
+                                        <a href={link.path}>{link.title}</a>
+                                    </li>
+                                ))}
                             </ul>
                         </div>
                         <div className={style.col}>
                             <h3 className="color-white">SITES</h3>
                             <ul className="d-flex flex-direction-column mt-20">
-                                <li>
-                                    <a href="">DC</a>
-                                </li>
-                                <li>
-                                    <a href="">MAD Magazine</a>
-                                </li>
-                                <li>
-                                    <a href="">CD Kids</a>
-                                </li>
-                                <li>
-                                    <a href="">DC Universe</a>
-                                </li>
-                                <li>
-                                    <a href="">DC Power Visa</a>
-                                </li>
+                                {sitesLinks.map((link, index) => (
+                                    <li key={index}>
+                                        <a href={link.path}>{link.title}</a>
+                                    </li>
+                                ))}  
                             </ul>
                         </div>
                         <div className={style.col40}>
@@ -109,26 +55,26 @@ export default function Footer() {
                 </div>
             </section>
             <section className="footer-bottom">
-            <div className={`py-20 ${style.footer}`}>
-                <div className="container d-flex justify-content-space-between align-center">
-                    <div className={style.btn}>SIGN-UP NOW!</div>
-                    <div>
-                        <h4>FOLLOW US</h4>
-                        <ul>
-                            <li>
-                                <a href="">
-                                    <Facebook />
-                                </a>
-                            </li>
-                            <li>
-                                <a href="">
+                <div className={`py-20 ${style.footer}`}>
+                    <div className="container d-flex justify-content-space-between align-center">
+                        <div className={style.btn}>SIGN-UP NOW!</div>
+                        <div>
+                            <h4>FOLLOW US</h4>
+                            <ul>
+                                <li>
+                                    <a href="">
+                                        <Facebook />
+                                    </a>
+                                </li>
+                                <li>
+                                    <a href="">
 
-                                </a>
-                            </li>
-                        </ul>
+                                    </a>
+                                </li>
+                            </ul>
+                        </div>
                     </div>
                 </div>
-            </div>
             </section>
         </footer>
     );
